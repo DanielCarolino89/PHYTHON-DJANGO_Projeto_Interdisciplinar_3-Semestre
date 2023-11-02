91ccdd873998cc007667af3e6dfca552d21a050a
Daniel Carolino <99697178+DanielCarolino89@users.noreply.github.com>
2023-11-01 21:11:00 -0300

    Add files via upload

---
+++ ods2/aplicativo/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('junte', views.junte, name='junte'),
+    path('apoio', views.apoio, name='apoio'),
+    path('parceiros', views.parceiros, name='parceiros'),
+    path('sobre', views.sobre, name='sobre'),
+]
