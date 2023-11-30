8f0c7f204d75d340cc1a8ca92c0525865dfa0289
Daniel Carolino <99697178+DanielCarolino89@users.noreply.github.com>
2023-11-29 21:18:45 -0300

    Delete ods2 directory

--- ods2/static/js/javascript.js
+++
@@ -1,9 +0,0 @@
-const sidebar = document.querySelector('.sidebar');
-
-const toggleSidebar = () => {
-  sidebar.classList.toggle('hidden');
-};
-
-const toggleSidebarButton = document.querySelector('#toggleSidebarButton');
-
-toggleSidebarButton.addEventListener('click', toggleSidebar);
