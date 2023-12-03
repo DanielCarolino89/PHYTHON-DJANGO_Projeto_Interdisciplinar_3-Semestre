b02e61e99e66c26587f8dbe38e8e38229e029ccc
Daniel Carolino <99697178+DanielCarolino89@users.noreply.github.com>
2023-12-02 22:20:10 -0300

    Delete ods2 directory

--- ods2/aplicativo/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from aplicativo.models import PessoaFisica,PessoaJuridica,PessoaApoio,ItemsDoacao
-
-admin.site.register(PessoaFisica),
-admin.site.register(PessoaJuridica),
-admin.site.register(PessoaApoio),
-admin.site.register(ItemsDoacao),
